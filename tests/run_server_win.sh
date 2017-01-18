5d8195cba238e2a2b9487ca9ae2e375060d46dc8
StefanA <as@prepress-digital.com>
2017-01-18 18:29:23 +0100

    windows script for db

---
+++ tests/run_server_win.sh
@@ -0,0 +1,4 @@
+set STARDOG_HOME=C:\Users\stefa\Dropbox\Studium\5.Semester\Vertiefungsseminar-git\RDFa_parser\tests\db
+
+start C:/Users/stefa/Dropbox/Studium/5.Semester/Vertiefungsseminar-git/RDFa_parser/tests/stardog/bin/stardog-admin server start
+start C:/Users/stefa/Dropbox/Studium/5.Semester/Vertiefungsseminar-git/RDFa_parser/tests/stardog/bin/stardog-admin db create -n test_db
